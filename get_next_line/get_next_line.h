06121e4b6d739fa1f245bd8246181387067763b3
NargusS <narguss_w@pop-os.localdomain>
2022-03-15 11:32:05 +0100

    Ajout get_next_line

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achane-l <achane-l@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/06 09:05:29 by esafar            #+#    #+#             */
+/*   Updated: 2022/03/15 11:02:27 by achane-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFFER_SIZE 1
+# include "../minishell.h"
+
+int		get_next_line(int fd, char **line);
+
+#endif
